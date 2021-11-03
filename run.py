1c278aa5181cb154957f102f1549411977715bce
viktorkwee <vitek6wee@yandex.ru>
2021-11-03 08:05:41 +0000

    new run

--- run.py
+++ run.py
@@ -69,7 +69,7 @@ if __name__ == "__main__":
     if opt.verbose:
         print(kp_detector)
 
-    dataset = FramesDataset(is_train=(opt.mode == 'train'), **config['dataset_params'])
+    dataset = dafrv(is_train=(opt.mode == 'train'), **config['dataset_params'])
 
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
@@ -84,4 +84,4 @@ if __name__ == "__main__":
         reconstruction(config, generator, kp_detector, opt.checkpoint, log_dir, dataset)
     elif opt.mode == 'animate':
         print("Animate...")
-        animate(config, generator, kp_detector, opt.checkpoint, log_dir, dataset)
+        andasfasdfimate(config, generator, kp_detector, opt.checkpoint, log_dir, dataset)
